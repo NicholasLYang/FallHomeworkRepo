0c5d021f2582b1bd38d9b90587199b3c3b41df7d
Nicholas Yang <nick@nicholasyang.com>
2014-09-23 08:01:54 -0400

    changed hw 3 folder name

---
+++ 03HW/Driver.java
@@ -0,0 +1,11 @@
+public class Driver 
+{
+    public static void main(String[] args)
+    {
+	String s = "nicholas yang";
+	int place = s.indexOf(" ");
+	System.out.println(place);
+	System.out.println(s.substring(9));
+	System.out.println(s.substring(0, 8));
+    }
+}
