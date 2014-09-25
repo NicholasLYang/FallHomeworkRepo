a02f84f4a91e65f0e9bc9bc3bc82e9f4268990d2
Nicholas Yang <nick@nicholasyang.com>
2014-09-24 22:17:25 -0400

    added lab work

--- 05HW/Driver.java
+++ 05HW/Driver.java
@@ -20,4 +20,45 @@ public class Driver
 	return a + b + b + a;
     }
 
+    public int diff21(int n) 
+    {
+	if (n > 21) 
+	    {
+		return ((n-21)*2);
+	    }
+	else 
+	    {
+		return (21 - n);
+	    }
+	return n;
+    }
+  
+    public boolean nearHundred(int n) 
+    {
+	if( Math.abs(100 - n) <= 10 ) return true;
+	else if ( Math.abs(200 - n) <= 10 ) return true;
+	else return false;
+    
+    }
+  
+  public boolean mixStart(String str) {
+    if (str.length() < 3) return false;
+   String s = str.substring(1, 3);
+   if (s.equals("ix")) return true;
+   
+  return false;
+  }
+  
+  public int teaParty(int tea, int candy) {
+    if (tea < 5 || candy < 5) return 0;
+    if (Math.abs(tea -candy) >= candy || Math.abs(tea -candy) >= tea ) return 2;
+    else return 1;
+  }
+  
+  public boolean lastDigit(int a, int b, int c) {
+    if (a % 10 == b % 10 || b % 10 == c % 10 || a % 10 == c % 10 ) return true;
+    else return false;
+  }
+}
+
 }
