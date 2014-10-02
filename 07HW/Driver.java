dc01eb9449b09d054c6288af94fef723575ab20c
Nicholas Yang <nick@nicholasyang.com>
2014-10-02 12:01:25 -0400

    updated hw 7

---
+++ 07HW/Driver.java
@@ -0,0 +1,11 @@
+public class Driver
+{
+    public static void main(String[] args)
+    {
+	Triangle t;
+	String s = new String();
+	t = new Triangle();
+	s = t.triangle2(5);
+	System.out.println(s);
+    }
+}
--- 07HW/Triangle.java
+++ 07HW/Triangle.java
@@ -15,15 +15,33 @@ public class Triangle
 		}
 	    return s;
     }
-    public String triangle2 (int h) {
+    public String triangle2 (int h) 
+    {
+	int height = h + 1;
 	String s = new String();
-	int height = 0;
-	while (height<h) {
-	    int x = 0
-	    while (h - x >1) {
-		s = s + " ";
-		x++;
+	int numOfAsterisks = 1;
+	int numOfSpaces;
+	int numOfA2;
+	while (numOfAsterisks < height) 
+	    {
+		numOfSpaces = height - numOfAsterisks;
+		while (numOfSpaces > 0) 
+		    {
+			s = s + " ";
+			numOfSpaces--;
+		    }
+		numOfA2 = numOfAsterisks;
+		while (numOfA2 > 0)
+		{
+		    s = s + "*";
+		    numOfA2--;
+		}
+		numOfAsterisks++;
+		s = s + "\n";
+		
 	    }
-	    
-	    
+	return s;
+
+    }
+
 }
