55ab1886a45a0be4455f97e6b7d2561b332a8c07
Nicholas Yang <nick@nicholasyang.com>
2014-10-26 19:07:07 -0400

    updated hw 13

--- 13HW/ArrayPractice.java
+++ 13HW/ArrayPractice.java
@@ -4,3 +4,19 @@ public int[] frontPiece(int[] nums) {
   int[] output = {nums[0], nums[1]};
   return output;
 }
+public int sum13(int[] nums) {
+int output = 0;
+int i = 0;
+   while (i < nums.length)
+   {
+     if (nums[i] == 13)
+     {i = i + 2;}
+     else
+     {
+     output = nums[i] + output;
+     i++;
+     }
+   }
+     
+  return output;
+}
