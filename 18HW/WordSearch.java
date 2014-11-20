3c7256918a284d23e874203de1fb8a4a4449a21f
Nicholas Yang <nick@nicholasyang.com>
2014-11-20 18:46:23 -0500

    added left diagonal because why not

--- 18HW/WordSearch.java
+++ 18HW/WordSearch.java
@@ -56,7 +56,53 @@ public class WordSearch{
 
 	return true;
     }
+    public boolean checkLD(String w, int row, int col)
+    {
+      	boolean print = true;
+	int r = row,c = col;
+	if (col + w.length() > board[0].length)
+	    {
+		return false;
+	    }
+	if (row + w.length() > board.length)
+	    {
+		return false;
+	    }
+	for (int i=0;i<w.length();i++){
+	    if (board[r][c] != '.') {
+		if(board[r][c] != w.charAt(i)){
+		    return false;
+		}
+	    }
+	    r++;
+	    c++;
+	    
+	}
+	
 
+	return true;
+    }
+    public void addWordLD(String w, int row, int col)
+    {
+	int r=row,c=col;
+	if(checkLD(w,r,c))
+	    {
+		for (int i = 0; i < w.length(); i++)
+		    {
+			board[r][c]=w.charAt(i);
+			r++;
+			c++;
+		    }
+	    }
+	else System.out.println("Conflict");
+    }
+    public void addReverseWordLD(String w, int row, int col)
+    {
+    
+       String reverse = new StringBuilder(w).reverse().toString();
+       this.addWordLD(reverse, row, col);
+    }
+    
     public void addWordH(String w, int row, int col){
 	int r=row,c=col;
 	if(checkH(w,r,c)){
@@ -120,11 +166,15 @@ public class WordSearch{
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
 	System.out.println(w);
-	w.addWordH("hello",3,5);
-	w.addReverseWordH("world",4,8);
-	w.addWordV("height", 3, 5);
-	w.addReverseWordV("help", 11, 12);
-				
+
+	
+	w.addWordH("help",3,5);
+	w.addReverseWordH("stuck",4,8);
+	w.addWordV("ina", 7, 5);
+	w.addReverseWordV("word", 6, 2);
+	w.addReverseWordLD("maker", 8, 8);
+      
+	w.addWordLD("maker", 1, 1);	
 	System.out.println(w);
     }
 }
